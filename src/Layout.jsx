74ac21ff13a09ea578d5977239e4499493b7afa2
liviaguirao <farialivia04@gmail.com>
2024-05-06 16:47:15 -0300

    layout contado

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,9 +1,10 @@
 import './Layout.css'
 
 function Layout() {
-    return <div className="div">
-       hello word
-    </div>
+    return <body>
+        <h1>Genius</h1>
+      
+    </body>
 
 }
    
