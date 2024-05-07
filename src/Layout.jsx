cf516234a45408c6d08d9170637f44b8b3e9f669
liviaguirao <farialivia04@gmail.com>
2024-05-06 23:22:11 -0300

    componentes para status de jogo adicionada

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,9 +1,16 @@
 import './Layout.css'
 
+
 function Layout() {
+    const nivel = 1;
+    const nivelMaximo = 1;
+
     return <body>
-        <h1>Genius</h1>
-      
+    <h1>GENIUS</h1>
+    <div className='status'>
+        <span>Nível: {nivel}</span>
+        <span>Nível máximo: {nivelMaximo}</span>
+    </div>
     </body>
 
 }
